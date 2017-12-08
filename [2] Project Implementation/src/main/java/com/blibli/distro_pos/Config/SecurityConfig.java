6cd8965f30dd0fd6b6e1dc07b7a25f94f1fc0263
Raditia Madya <gustiraditia@gmail.com>
2017-12-09 00:52:13 +0700

    update UI

--- [2] Project Implementation/src/main/java/com/blibli/distro_pos/Config/SecurityConfig.java
+++ [2] Project Implementation/src/main/java/com/blibli/distro_pos/Config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
 //                .antMatchers().permitAll()
-                .antMatchers("/admin","/add_user","/view_user", "/admin2", "/add_user2", "/item")
+                .antMatchers("/admin","/add_user","/view_user", "/admin2", "/add_user2", "/item",
+                        "/discount")
                     .access("hasAnyAuthority('MANAGER')")
                 .antMatchers("/dashboard").access("hasAnyAuthority('CASHIER', 'MANAGER')")
                 .and()
